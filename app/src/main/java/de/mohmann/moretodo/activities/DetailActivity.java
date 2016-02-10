b2614998aa6bc2127ea26ad4dd1b259dc152be88
Martin Ohmann <martin@mohmann.de>
2016-02-10 16:00:45 +0100

    disable 'mark as done' menu item if todo is already done

--- app/src/main/java/de/mohmann/moretodo/activities/DetailActivity.java
+++ app/src/main/java/de/mohmann/moretodo/activities/DetailActivity.java
@@ -162,6 +162,11 @@ public class DetailActivity extends AppCompatActivity implements DialogInterface
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
+
+        if (mTodo != null && mTodo.isDone()) {
+            MenuItem item = menu.findItem(R.id.action_mark_done);
+            item.setEnabled(false);
+        }
         return true;
     }
 
